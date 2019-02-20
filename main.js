d910343144b1ef742ec08cc3fa32e9a73e364bf4
schaerta <tatiana@govolunteer.com>
2019-02-20 12:10:15 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    inventory: 9,
+    description: 'Beautiful and soft.',
+    image: './img/socks.jpeg',
+    moreProductsLink: 'https://www.ecosia.org/',
+    MoreProductsText: 'Search for more products...'
+  }
+})

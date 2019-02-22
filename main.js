e4249e322b8c301047e0dd4957ef98208744b44c
schaerta <tatiana@govolunteer.com>
2019-02-22 22:36:06 +0100

    Added filters for Complete and Incomplete Tasks

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    tasks: [],
-    newTask: ''
+    el: '#app',
+    data: {
+    tasks: [
+        { description: "Brush teeth", completed: false, show: true },
+        { description: "Go to dentist", completed: true, show: true }
+    ],
+    newTask: '',
+    showCompleted: true,
+    showNotCompleted: true
   },
+
   methods: {
-    addTask() {
-      this.tasks.push(this.newTask);
-      // Empty out the task so the input field gets cleared
-      this.newTask = '';
-    }
+      addTask() {
+          this.tasks.push(
+              { description: this.newTask, completed: false }
+          );
+          // Empty out the task so the input field gets cleared
+          this.newTask = '';
+      },
+      filterOpen() {
+          for (let i=0; i < this.tasks.length; i++){
+              if (this.tasks[i].completed) {
+                  this.tasks[i].show = false;
+              }
+          }
+      },
+      filterCompleted() {
+          for (let i=0; i < this.tasks.length; i++){
+              if (!this.tasks[i].completed) {
+                  this.tasks[i].show = false;
+              }
+          }
+      },
+      showAllTasks() {
+          for (let i=0; i < this.tasks.length; i++){
+              this.tasks[i].show = true;
+          }
+      }
+  },
+
+  computed: {
+      incompleteTasks() {
+          return this.tasks.filter(task => !task.completed);
+      }
   }
 })
